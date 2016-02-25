d62c8a9dafca1358504f6d9491de7192468f47cc
Jesse <jmendez7@gmu.edu>
2016-02-25 17:51:46 -0500

    clouds

--- js/main.js
+++ js/main.js
@@ -15,9 +15,11 @@ window.onload = function() {
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
+    // Load Resources
     function preload()
     {
         game.load.image('darkness', 'assets/pics/darkness.jpg');
+        game.load.image('cloud', 'assets/pics/cloud.png');
         game.load.image('raindrop', 'assets/pics/raindrop.png');
         
         game.load.spritesheet('chara', 'assets/sprites/chara.png', 56, 75);
@@ -26,60 +28,43 @@ window.onload = function() {
         game.load.audio('theme', 'assets/audio/Here We Are.mp3');
     }
     
+    // Player Attributes
     var controls;
     var player;
     var lastDirection;
     
-    var background;
-    
+    // Enviorment
     var rain;
     var raindrop1;
     var raindrop2;
     var raindrop3;
+    var raindrop4;
+    var raindrop5;
+    var raindrop6;
     
+    // Sound
     var theme
     
+    // Creates Game
     function create()
     {
         // Size of World
         game.world.resize(1600, 900);
         
-        // Background
-        background = game.add.image(0, 0, 'darkness');
-        
         // Enable Arcade Physics
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        // Rain
-        rain = game.add.group();
+        // Background
+        var background = game.add.image(0, 0, 'darkness');
         
-        // Rain Physics Enable
-        rain.enableBody = true;
+        // Storm Enviorment
+        stormSetup();
         
-        // Raindrop
-        raindrop1 = rain.create(0, 0, 'raindrop');
-        raindrop1.body.gravity.y = 2000;
-        raindrop2 = rain.create(660, 0, 'raindrop');
-        raindrop2.body.gravity.y = 1000;
-        raindrop3 = rain.create(420, 0, 'raindrop');
-        raindrop3.body.gravity.y = 500;
-        
-        // Creates the player
-        player = game.add.sprite(0, game.world.height, 'chara', 1);
-        
-        // Player's Physics
-        game.physics.arcade.enable(player);
-        player.body.collideWorldBounds = true;
-        player.body.gravity.y = 250;
-        
-        // Player's Movements
-        player.animations.add('left', [11, 10, 9, 8]);
-        player.animations.add('right', [4, 5, 6, 7]);
+        // Player Setup
+        playerSetup();        
         
         // Keyboard controls
         controls = game.input.keyboard.createCursorKeys();
-        
-        game.camera.follow(player);
 
         
         // Music
@@ -87,10 +72,43 @@ window.onload = function() {
         theme.play();
     }
     
+    // Updates Game
     function update()
     {   
         rainEffects();
         playerMovements();
+    }
+    
+    // Rain Enviorment
+    function stormSetup()
+    {
+        // Clouds
+        var clouds = game.add.group();
+        for (var i; i < 4; i++)
+        {
+            var cloud = clouds.create(0, 0, 'cloud');
+        }
+        
+        // Rain
+        rain = game.add.group();
+        
+        // Rain Physics Enable
+        rain.enableBody = true;
+        
+        // Raindrops
+        raindrop1 = rain.create(0, 0, 'raindrop');
+        raindrop1.body.gravity.y = 500;
+        raindrop2 = rain.create(320, 0, 'raindrop');
+        raindrop2.body.gravity.y = 1500;
+        raindrop3 = rain.create(640, 0, 'raindrop');
+        raindrop3.body.gravity.y = 3000;
+        raindrop4 = rain.create(960, 0, 'raindrop');
+        raindrop4.body.gravity.y = 3000;
+        raindrop5 = rain.create(1280, 0, 'raindrop');
+        raindrop5.body.gravity.y = 1500;
+        raindrop6 = rain.create(1600, 0, 'raindrop');
+        raindrop6.body.gravity.y = 500;
+        
     }
     
     // Rain Effects
@@ -114,6 +132,23 @@ window.onload = function() {
         } 
     }
     
+    // Player Setup
+    function playerSetup()
+    {
+        // Creates the player
+        player = game.add.sprite(0, game.world.height, 'chara', 1);
+        
+        // Player's Physics
+        game.physics.arcade.enable(player);
+        player.body.collideWorldBounds = true;
+        player.body.gravity.y = 250;
+        
+        // Player's Movements
+        player.animations.add('left', [11, 10, 9, 8]);
+        player.animations.add('right', [4, 5, 6, 7]);
+        
+        game.camera.follow(player);
+    }
     // Player's Movement Control
     function playerMovements()
     {   
@@ -140,7 +175,7 @@ window.onload = function() {
         // Vertical Movements
         else if (controls.up.isDown)
             player.body.velocity.y = -150;
-        else if(controls.down.isDown)
+        else if (controls.down.isDown)
         {
             player.frame = 1;
             
